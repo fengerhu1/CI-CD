5a4ff2b73619798d502adcf9803ed02dc4fb2f9f
WhirlFourEye <13735887700@163.com>
2018-05-23 10:06:49 +0800

    test file update

--- src/App.test.js
+++ src/App.test.js
@@ -7,3 +7,20 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('renders correct', () => {
+  const renderer = new ShallowRenderer();
+  renderer.render(<App />);
+  const result = renderer.getRenderOutput();
+
+  expect(result.type).toBe('div');
+  expect(result.props.children).toEqaul([
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1 className="App-title">Welcome to React</h1>
+    </header>,
+    <p className="App-intro">
+      To get started, edit <code>src/App.js</code> and save to reload.
+    </p>
+  ]);
+})
